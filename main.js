6eaa4f89c12f843fb78a428d4f067af7d30c84cf
Justin Kim <jjkim928@gmail.com>
2017-08-08 11:24:00 -0400

    ok...added a really crappy eventListener for the input

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let url = "http://www.recipepuppy.com/about/api/";
 
 let search_bar = document.getElementById("search_bar");
+search_bar.addEventListener("keyup", search);
+
+function search() {
+	let user_input = this.value;
+	console.log(user_input);
+}
+
