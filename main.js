1b2a26e4527479d43e53893c7e9716f5f8b9fada
Justin Kim <jjkim928@gmail.com>
2017-08-08 11:54:35 -0400

    used template literal to concatenate API URL with user's search term

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-let url = "http://www.recipepuppy.com/about/api/";
+let url = "http://www.recipepuppy.com/api/?q=";
 
 let search_bar = document.getElementById("search_bar");
 let btnSearch = document.getElementById("btnSearch");
 
 function search() {
 	let user_input = search_bar.value;
-	console.log(user_input);
+	console.log(user_input); // test to make sure something is being logged to console...
+
+	let search_term = `
+		${url}${user_input}
+	`;
+
+	console.log(search_term);
+
+
+
 }
 
 
