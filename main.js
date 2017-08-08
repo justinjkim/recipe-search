8ee224e150ff514aa29e02c9502986f8ff2355ca
Justin Kim <jjkim928@gmail.com>
2017-08-08 13:08:55 -0400

    ok, got the fetch to pull in the data

--- main.js
+++ main.js
@@ -1,17 +1,72 @@
-let url = "http://www.recipepuppy.com/api/?q=";
+
+
+let url = "http://recipepuppyproxy.herokuapp.com/api/?q="
 
 let search_bar = document.getElementById("search_bar");
 let btnSearch = document.getElementById("btnSearch");
 
 function search() {
 	let user_input = search_bar.value;
-	console.log(user_input); // test to make sure something is being logged to console...
-
 	let search_term = `
 		${url}${user_input}
 	`;
+	console.log(search_term); // ok, it's definitely logging the proper url
 
-	console.log(search_term);
+	fetch(search_term)
+	  .then(
+	    // The promise returns a response from the server.
+	    function(response) {
+	      // We process the response accordingly.
+	      if (response.status !== 200) {
+	        console.log("Error: " + response.status);
+	        return;
+	      }
+	      else {
+	      	response.json().then(function(data) {
+	      		// log the results of search term
+	      		let data_copy = data.results;
+
+	        	for (i in data_copy) {
+	        		let recipe = document.createElement("div");
+	        		recipe.setAttribute("class", "recipe");
+	        		let main = document.querySelector("main");
+	        		main.appendChild(recipe);
+
+	        		console.log(data_copy[i].title);
+	        		// template literal
+	        		let innards = `
+	        			<h3>${data_copy[i].title}</h3>
+	        			<img src=${data_copy[i].thumbnail} />
+	        		`
+
+	        		recipe.innerHTML = innards;
+	        	} // end of for loop
+
+
+	        	// not sure why but Map doesn't work here...
+	        	// data.map(content) => {
+	        	// 	let recipe = document.createElement("div");
+	        	// 	recipe.setAttribute("class", "recipe");
+	        	// 	let main = document.querySelector("main");
+	        	// 	main.appendChild(recipe);
+
+	        	// 	// template literal
+	        	// 	let innards = `
+	        	// 		<img src="${content.results.href}" />
+	        	// 		<h3>${content.results.title}</h3>
+	        	// 	`
+
+	        	// 	recipe.innerHTML = innards;
+	        	// } // end of map function
+
+	      	}); // end of actual function that parses through JSON
+	      } // end of overall successful response function
+	      
+	    } // end of overall ALL response functions
+	  ) // end of "then"
+	  .catch(function(err) {
+	    console.log("Fetch Error :-S", err);
+	  });
 
 
 
